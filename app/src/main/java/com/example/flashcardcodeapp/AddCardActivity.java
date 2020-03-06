dd408a22e7282afd978b03132d665f9715a6301b
Neunis <n_nunis@u.pacific.edu>
2020-03-05 19:24:05 -0800

    Imported Cancel/Clear button icon Cancel button on click goes back to MainActivity

--- app/src/main/java/com/example/flashcardcodeapp/AddCardActivity.java
+++ app/src/main/java/com/example/flashcardcodeapp/AddCardActivity.java
@@ -2,7 +2,9 @@ package com.example.flashcardcodeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -11,6 +13,14 @@ public class AddCardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
 
+        findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(AddCardActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
 
     }
 }
