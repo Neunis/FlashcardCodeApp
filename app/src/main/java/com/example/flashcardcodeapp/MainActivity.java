40a8e7a1778872e577bd77d92796375f4aaf9ae4
Neunis <n_nunis@u.pacific.edu>
2020-03-11 11:42:59 -0700

    Users can browse multiple cards:
    - Created index var to keep track of index of current card
    - Imported right arrow and adjusted format on view
    - Connected next button for view model flow
    - Arrow moves to next card upon being clicked
    - Iterates through all cards, then resets when done to avoid error
    - Q&A are set from db
    - Functions created to clean up MainActivity for readability

--- app/src/main/java/com/example/flashcardcodeapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardcodeapp/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     FlashcardDatabase flashcardDatabase;
     List<Flashcard> allFlashcards;
 
+    int currentCardDisplayedIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,39 +26,13 @@ public class MainActivity extends AppCompatActivity {
         //local var with list of flashcards, saves data to the database (db)
         allFlashcards = flashcardDatabase.getAllCards();
 
-        //fxn checks db to see if there are any saved flashcards b4 showing default
-        if (allFlashcards != null && allFlashcards.size() > 0) {
-            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
-            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        flashcardDbCheck();
 
-        }
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-            }
-        });
+        flashcardView();
 
-        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
-            }
+        nextButtonAction();
 
-
-        });
-
-
-        findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                startActivityForResult(intent, 100);
-            }
-
-        });
+        addButtonAction();
 
     }
 
@@ -78,6 +54,69 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
+
+        }
+    }
+
+    //functions for flashcard in general (view, etc.)
+    private void flashcardView(){
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+            }
+
+
+        });
+    }
+
+    //functions for button actions
+    private void addButtonAction(){
+        findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                startActivityForResult(intent, 100);
+            }
+
+        });
+    }
+
+    private void nextButtonAction(){
+        findViewById(R.id.nextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //progress to next card with addition of pointer index
+                currentCardDisplayedIndex++;
+
+                //checks to make sure that index stays in bounds if viewing last card in list
+                if(currentCardDisplayedIndex > allFlashcards.size() -1){
+                    currentCardDisplayedIndex = 0;
+                }
+
+                //sets Q&A TextViews w/ data from the db
+                ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+
+            }
+        });
+    }
+
+    //functions for database
+    private void flashcardDbCheck(){
+        //fxn checks db to see if there are any saved flashcards b4 showing default
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
 
         }
     }
